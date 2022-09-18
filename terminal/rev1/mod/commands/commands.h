da143393c0f5b83fee852479c77fe7ccdbe92fec
Colton Acosta <cacost12@asu.edu>
2022-09-18 11:28:25 -0700

    created new response code for ping/connect commands

--- terminal/rev1/mod/commands/commands.h
+++ terminal/rev1/mod/commands/commands.h
@@ -23,7 +23,6 @@ extern "C" {
 #define PING_OP 	0x01 /* ping command opcode    */
 #define CONNECT_OP	0x02 /* connect command opcode */
 #define IGNITE_OP	0x20 /* connect command opcode */
-#define POWER_OP    0x21 /* power command opcode   */
 
 /*------------------------------------------------------------------------------
  Function Prototypes 
