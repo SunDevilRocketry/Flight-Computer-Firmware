231d3d8036f53c0ace9d8c0008cbe94c3b4d0bd1
Colton Acosta <cacost12@asu.edu>
2022-10-09 15:37:37 -0700

    cleaned hal msp auto-generated code

--- blink/rev1/main.c
+++ blink/rev1/main.c
@@ -199,12 +199,16 @@ while (1)
   * @param  line: assert_param error line source number
   * @retval None
   */
-void assert_failed(uint8_t *file, uint32_t line)
+void assert_failed
+	(
+	uint8_t *file, 
+	uint32_t line
+	)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
+} /* assert_failed */
 #endif /* USE_FULL_ASSERT */
 
+
+/*******************************************************************************
+* END OF FILE                                                                  * 
+*******************************************************************************/
--- blink/rev1/main.h
+++ blink/rev1/main.h
@@ -1,22 +1,12 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.h
-  * @brief          : Header for main.c file.
-  *                   This file contains the common defines of the application.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
+/*******************************************************************************
+*                                                                              *
+* PROCEDURE:                                                                   *
+*       GPIO_Init                                                              * 
+*                                                                              *
+* DESCRIPTION:                                                                 * 
+*       Initializes all GPIO pins and sets alternate functions                 *
+*                                                                              *
+*******************************************************************************/
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __MAIN_H
@@ -26,40 +16,21 @@
 extern "C" {
 #endif
 
-/* Includes ------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------
+Includes                                                                    
+------------------------------------------------------------------------------*/
 #include "stm32h7xx_hal.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
 
-/* USER CODE END Includes */
+/*------------------------------------------------------------------------------
+Macros  
+------------------------------------------------------------------------------*/
 
-/* Exported types ------------------------------------------------------------*/
-/* USER CODE BEGIN ET */
 
-/* USER CODE END ET */
-
-/* Exported constants --------------------------------------------------------*/
-/* USER CODE BEGIN EC */
-
-/* USER CODE END EC */
-
-/* Exported macro ------------------------------------------------------------*/
-/* USER CODE BEGIN EM */
-
-/* USER CODE END EM */
-
-/* Exported functions prototypes ---------------------------------------------*/
+/*------------------------------------------------------------------------------
+Exported functions prototypes                                             
+------------------------------------------------------------------------------*/
 void Error_Handler(void);
-
-/* USER CODE BEGIN EFP */
-
-/* USER CODE END EFP */
-
-/* Private defines -----------------------------------------------------------*/
-/* USER CODE BEGIN Private defines */
-
-/* USER CODE END Private defines */
 
 #ifdef __cplusplus
 }
