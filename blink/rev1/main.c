c7696cb5ec441ea43b926613cd3509cfe933773a
Colton Acosta <cacost12@asu.edu>
2022-10-09 15:27:53 -0700

    cleaned up main.c code

--- blink/rev1/main.c
+++ blink/rev1/main.c
@@ -1,193 +1,195 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
+/*******************************************************************************
+*
+* FILE: 
+*       main.c
+*
+* DESCRIPTION: 
+*		Flashes the onboard rgb led in order to verify that the flight computer 
+*       board and programmer are functioning correctly 
+*
+*******************************************************************************/
+
+
+/*------------------------------------------------------------------------------
+Standard Includes                                                                     
+------------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+Project Includes                                                                     
+------------------------------------------------------------------------------*/
+
+/* Pin definitions and main prototypes */
 #include "main.h"
+#include "sdr_pin_defines_A0002_rev1.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
+/* SDR Modules */
 
-/* USER CODE END Includes */
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
+/*------------------------------------------------------------------------------
+Global Variables                                                                  
+------------------------------------------------------------------------------*/
 
-/* USER CODE END PTD */
 
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-/* USER CODE END PD */
+/*------------------------------------------------------------------------------
+Typedefs                                                                  
+------------------------------------------------------------------------------*/
 
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
 
-/* USER CODE END PM */
+/*------------------------------------------------------------------------------
+Function prototypes                                                          
+------------------------------------------------------------------------------*/
+void        SystemClock_Config( void );
+static void GPIO_Init         ( void );
 
-/* Private variables ---------------------------------------------------------*/
 
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
+/*------------------------------------------------------------------------------
+Application entry point                                                      
+------------------------------------------------------------------------------*/
+int main
+	(
+	void
+	)
 {
-  /* USER CODE BEGIN 1 */
+/*------------------------------------------------------------------------------
+MCU Initialization                                                                  
+------------------------------------------------------------------------------*/
+HAL_Init();
+SystemClock_Config();
+GPIO_Init();
 
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
-}
-
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  /** Supply configuration update enable
-  */
-  HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
-
-  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_DIV1;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
-                              |RCC_CLOCKTYPE_D3PCLK1|RCC_CLOCKTYPE_D1PCLK1;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV1;
-  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11, GPIO_PIN_RESET);
-
-  /*Configure GPIO pins : PA9 PA10 PA11 */
-  GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-}
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
+/*------------------------------------------------------------------------------
+Event Loop                                                                  
+------------------------------------------------------------------------------*/
+while (1)
   {
   }
-  /* USER CODE END Error_Handler_Debug */
-}
+
+} /* main.c */
+
+
+/*******************************************************************************
+*                                                                              *
+* PROCEDURE:                                                                   * 
+*       SystemClock_Config                                                     *
+*                                                                              *
+* DESCRIPTION:                                                                 * 
+*       Initializes the microcontroller clock. Enables peripheral clocks and   *
+*       sets prescalers                                                        *
+*                                                                              *
+*******************************************************************************/
+void SystemClock_Config
+	(
+	void
+	)
+{
+/* Init structs */
+RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+/* Supply configuration update enable */
+HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
+
+/* Configure the main internal regulator output voltage */
+__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+while( !__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY) ) 
+	{
+	/* Wait for PWR_FLAG_VOSRDY flag */
+	}
+
+/* Initializes the RCC Oscillators according to the specified parameters 
+   in the RCC_OscInitTypeDef structure. */
+RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI;
+RCC_OscInitStruct.HSIState            = RCC_HSI_DIV1;
+RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_NONE;
+if ( HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK )
+	{
+	Error_Handler();
+	}
+
+/* Initializes the CPU, AHB and APB buses clocks */
+RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK    |
+                                   RCC_CLOCKTYPE_SYSCLK  |
+						           RCC_CLOCKTYPE_PCLK1   |
+                                   RCC_CLOCKTYPE_PCLK2   |
+						           RCC_CLOCKTYPE_D3PCLK1 |
+								   RCC_CLOCKTYPE_D1PCLK1;
+RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_HSI;
+RCC_ClkInitStruct.SYSCLKDivider  = RCC_SYSCLK_DIV1;
+RCC_ClkInitStruct.AHBCLKDivider  = RCC_HCLK_DIV1;
+RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV1;
+RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV1;
+RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV1;
+RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV1;
+
+if ( HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK )
+	{
+	Error_Handler();
+	}
+
+} /* SystemClock_Config */
+
+
+/*******************************************************************************
+*                                                                              *
+* PROCEDURE:                                                                   *
+*       GPIO_Init                                                              * 
+*                                                                              *
+* DESCRIPTION:                                                                 * 
+*       Initializes all GPIO pins and sets alternate functions                 *
+*                                                                              *
+*******************************************************************************/
+static void GPIO_Init
+	(
+	void
+	)
+{
+/* Init Struct */
+GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+/* GPIO Ports Clock Enable */
+__HAL_RCC_GPIOA_CLK_ENABLE();
+
+/*Configure GPIO pin Output Level */
+HAL_GPIO_WritePin( STATUS_GPIO_PORT, 
+                   STATUS_B_PIN |
+                   STATUS_G_PIN |
+                   STATUS_R_PIN    , 
+                   GPIO_PIN_RESET );
+
+/*Configure GPIO pins : PA9 PA10 PA11 */
+GPIO_InitStruct.Pin   = STATUS_B_PIN |
+                        STATUS_G_PIN |
+					    STATUS_R_PIN;
+GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_OD;
+GPIO_InitStruct.Pull  = GPIO_NOPULL;
+GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+HAL_GPIO_Init( STATUS_GPIO_PORT, &GPIO_InitStruct);
+
+} /* GPIO_Init */
+
+
+/*******************************************************************************
+*                                                                              *
+* PROCEDURE:                                                                   *
+*       Error_Handler                                                          * 
+*                                                                              *
+* DESCRIPTION:                                                                 * 
+* 		This function is executed in case of error occurrence                  *
+*                                                                              *
+*******************************************************************************/
+void Error_Handler
+	(
+	void
+	)
+{
+__disable_irq();
+while (1)
+	{
+	}
+} /* Error_Handler */
 
 #ifdef  USE_FULL_ASSERT
 /**
