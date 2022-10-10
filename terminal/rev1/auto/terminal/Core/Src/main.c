4d04673c955a3cfa661877ce9e1c5158d6fc5e36
Colton Acosta <cacost12@asu.edu>
2022-10-09 19:36:44 -0700

    maxed out cpu frequency using external oscillator

--- terminal/rev1/auto/terminal/Core/Src/main.c
+++ terminal/rev1/auto/terminal/Core/Src/main.c
@@ -117,16 +117,24 @@ void SystemClock_Config(void)
   HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
   /** Configure the main internal regulator output voltage
   */
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
 
   while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_DIV1;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 2;
+  RCC_OscInitStruct.PLL.PLLN = 80;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_3;
+  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+  RCC_OscInitStruct.PLL.PLLFRACN = 0;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -136,15 +144,15 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
                               |RCC_CLOCKTYPE_D3PCLK1|RCC_CLOCKTYPE_D1PCLK1;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV1;
-  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV1;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
   {
     Error_Handler();
   }
@@ -208,6 +216,7 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
--- terminal/rev1/main.c
+++ terminal/rev1/main.c
@@ -173,7 +173,7 @@ RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
 
 /* Configure the main internal regulator output voltage */
-__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
 while( !__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY) ) 
 	{
 	/* Wait for PWR_FLAG_VOSRDY flag */
@@ -181,10 +181,18 @@ while( !__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY) )
 
 /* Initializes the RCC Oscillators according to the specified parameters
    in the RCC_OscInitTypeDef structure. */
-RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI;
-RCC_OscInitStruct.HSIState            = RCC_HSI_DIV1;
-RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_NONE;
+RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+RCC_OscInitStruct.HSEState       = RCC_HSE_ON;
+RCC_OscInitStruct.PLL.PLLState   = RCC_PLL_ON;
+RCC_OscInitStruct.PLL.PLLSource  = RCC_PLLSOURCE_HSE;
+RCC_OscInitStruct.PLL.PLLM       = 2;
+RCC_OscInitStruct.PLL.PLLN       = 80;
+RCC_OscInitStruct.PLL.PLLP       = 2;
+RCC_OscInitStruct.PLL.PLLQ       = 2;
+RCC_OscInitStruct.PLL.PLLR       = 2;
+RCC_OscInitStruct.PLL.PLLRGE     = RCC_PLL1VCIRANGE_3;
+RCC_OscInitStruct.PLL.PLLVCOSEL  = RCC_PLL1VCOWIDE;
+RCC_OscInitStruct.PLL.PLLFRACN   = 0;
 if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
 	{
 	Error_Handler();
@@ -197,15 +205,15 @@ RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK    |
 	                               RCC_CLOCKTYPE_PCLK2   |
                                    RCC_CLOCKTYPE_D3PCLK1 |
 	                               RCC_CLOCKTYPE_D1PCLK1;
-RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_HSI;
+RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
 RCC_ClkInitStruct.SYSCLKDivider  = RCC_SYSCLK_DIV1;
 RCC_ClkInitStruct.AHBCLKDivider  = RCC_HCLK_DIV2;
-RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV1;
-RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV1;
-RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV1;
-RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV1;
+RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
+RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
+RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
 
-if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
 	{
 	Error_Handler();
 	}
@@ -333,7 +341,7 @@ __HAL_RCC_GPIOA_CLK_ENABLE();
 __HAL_RCC_GPIOC_CLK_ENABLE();
 //__HAL_RCC_GPIOB_CLK_ENABLE();
 //__HAL_RCC_GPIOE_CLK_ENABLE();
-//__HAL_RCC_GPIOH_CLK_ENABLE();
+__HAL_RCC_GPIOH_CLK_ENABLE();
 
 
 /*--------------------------- LED MCU PINS -----------------------------------*/
