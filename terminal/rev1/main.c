71618d88cd7ff547287d6dde70e06d0c1807dea4
Colton Acosta <cacost12@asu.edu>
2022-10-29 18:33:11 -0700

    renamed MX_I2C2_Init to IMU_GPS_I2C_Init

--- terminal/rev1/main.c
+++ terminal/rev1/main.c
@@ -44,8 +44,7 @@
 ------------------------------------------------------------------------------*/
 UART_HandleTypeDef huart6;  /* USB                               */
 I2C_HandleTypeDef  hi2c1;   /* Baro sensor                       */
-I2C_HandleTypeDef  hi2c2;   /* IMU I2C handler struct            */
-
+I2C_HandleTypeDef  hi2c2;   /* IMU and GPS                       */
 //SPI_HandleTypeDef  hspi2;  /* SPI handler struct for flash chip */
 
 
@@ -56,8 +55,7 @@ void	    SystemClock_Config ( void ); /* clock configuration               */
 static void GPIO_Init          ( void ); /* GPIO configurations               */
 static void USB_UART_Init      ( void ); /* USB UART configuration            */
 static void Baro_I2C_Init      ( void ); /* Baro sensor I2C configuration     */
-static void MX_I2C2_Init       ( void );
-
+static void IMU_GPS_I2C2_Init  ( void ); /* IMU/GPS I2C configuration         */
 //static void FLASH_SPI_Init     ( void ); /* FLASH SPI configuration           */
 
 
@@ -88,7 +86,7 @@ SystemClock_Config(); /* System clock                                         */
 GPIO_Init();          /* GPIO                                                 */
 USB_UART_Init();      /* USB UART                                             */
 Baro_I2C_Init();      /* Barometric pressure sensor                           */
-MX_I2C2_Init();
+IMU_GPS_I2C2_Init();  /* IMU and GPS                                          */
 //FLASH_SPI_Init();     /* External flash chip                                  */
 
 
@@ -235,53 +233,51 @@ if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
 } /* SystemClock_Config */
 
 
-/**
-* @brief I2C2 Initialization Function
-* @param None
-* @retval None
-*/
-static void MX_I2C2_Init(void)
+/*******************************************************************************
+*                                                                              *
+* PROCEDURE:                                                                   * 
+* 		IMU_GPS_I2C_Init                                                       *
+*                                                                              *
+* DESCRIPTION:                                                                 * 
+* 		Initializes the microcontroller I2C Interface for the IMU and GPS      *
+*                                                                              *
+*******************************************************************************/
+static void IMU_GPS_I2C2_Init
+	(
+	void
+	)
 {
 
-/* USER CODE BEGIN I2C2_Init 0 */
-
-/* USER CODE END I2C2_Init 0 */
-
-/* USER CODE BEGIN I2C2_Init 1 */
-
-/* USER CODE END I2C2_Init 1 */
-hi2c2.Instance = I2C2;
-hi2c2.Init.Timing = 0x307075B1;
-hi2c2.Init.OwnAddress1 = 0;
-hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-hi2c2.Init.OwnAddress2 = 0;
+/* I2C configuration settings */
+hi2c2.Instance              = I2C2;
+hi2c2.Init.Timing           = 0x307075B1;
+hi2c2.Init.OwnAddress1      = 0;
+hi2c2.Init.AddressingMode   = I2C_ADDRESSINGMODE_7BIT;
+hi2c2.Init.DualAddressMode  = I2C_DUALADDRESS_DISABLE;
+hi2c2.Init.OwnAddress2      = 0;
 hi2c2.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
-hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+hi2c2.Init.GeneralCallMode  = I2C_GENERALCALL_DISABLE;
+hi2c2.Init.NoStretchMode    = I2C_NOSTRETCH_DISABLE;
+
+/* Apply settings */
 if (HAL_I2C_Init(&hi2c2) != HAL_OK)
 	{
 	Error_Handler();
 	}
-/** Configure Analogue filter
-*/
+
+/* Configure Analogue filter */
 if (HAL_I2CEx_ConfigAnalogFilter(&hi2c2, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
 	{
 	Error_Handler();
 	}
 
-/** Configure Digital filter
-*/
+/* Configure Digital filter */
 if (HAL_I2CEx_ConfigDigitalFilter(&hi2c2, 0) != HAL_OK)
 	{
 	Error_Handler();
 	}
-/* USER CODE BEGIN I2C2_Init 2 */
 
-/* USER CODE END I2C2_Init 2 */
-
-}
-
+} /* IMU_GPS_I2C_Init */
 
 
 /*******************************************************************************
