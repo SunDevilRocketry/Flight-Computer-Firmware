f4a32a2150126be30a8e298472401e7bba6a6361
Colton Acosta <cacost12@asu.edu>
2022-10-30 22:02:30 -0700

    moved modules to top level directory to share with application code

--- mod/ignition/ignition.c
+++ mod/ignition/ignition.c
@@ -20,7 +20,7 @@
  Project Includes                                                                     
 ------------------------------------------------------------------------------*/
 #include "main.h"
-#include "sdr_pin_defines_A0002_rev1.h"
+#include "sdr_pin_defines_A0002.h"
 #include "ignition.h"
 
 
