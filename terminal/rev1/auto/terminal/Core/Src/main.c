ef51a0e57dbab322b2b0cdebaf6ce983b14afa30
Colton Acosta <cacost12@asu.edu>
2022-12-22 14:34:12 -0700

    added flash hold pin to auto code

--- terminal/rev1/auto/terminal/Core/Src/main.c
+++ terminal/rev1/auto/terminal/Core/Src/main.c
@@ -387,6 +387,9 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12|GPIO_PIN_6, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : PE4 */
@@ -415,8 +418,8 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PD12 PD6 */
-  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_6;
+  /*Configure GPIO pins : PD12 PD13 PD6 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_6;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
