9e6115783ca6bc336bb912d55b47398a486f47e5
niekky <trungkiencfc@gmail.com>
2023-04-15 16:35:56 -0700

    - Finished string converter testing function implementation - 3 test cases passed successfully

--- test/mod/sd_card/test_string_converter.c
+++ test/mod/sd_card/test_string_converter.c
@@ -32,18 +32,7 @@ Global Variables
 /*------------------------------------------------------------------------------
 Macros
 ------------------------------------------------------------------------------*/
-#define NUM_TESTS_readings_to_bytes ( 2 )
-
-
-/*------------------------------------------------------------------------------
-Types
-------------------------------------------------------------------------------*/
-struct sensor_data_test 
-{
-uint16_t imu_data[10],
-float	 pres_data[2]
-}
-
+#define NUM_TESTS_readings_to_bytes ( 3 )
 
 /*------------------------------------------------------------------------------
 Procedures 
@@ -99,36 +88,14 @@ void test_dataframe_to_string
     )
 {
 /*------------------------------------------------------------------------------
-Local Variables
-------------------------------------------------------------------------------*/
-SENSOR_DATA sensor_data;
-/*------------------------------------------------------------------------------
 Initializations
 ------------------------------------------------------------------------------*/
-// readings_to_bytes__test_type test_data[] = 
-// { 
-// #include "test_cases/readings_to_bytes.txt"
-// };
-
-sensor_data.imu_data.accel_x = 1;
-sensor_data.imu_data.accel_y = 2;
-sensor_data.imu_data.accel_z = 3;
-sensor_data.imu_data.gyro_x = 4;
-sensor_data.imu_data.gyro_y = 5;
-sensor_data.imu_data.gyro_z = 6;
-sensor_data.imu_data.mag_x = 7;
-sensor_data.imu_data.mag_y = 8;
-sensor_data.imu_data.mag_z = 9;
-sensor_data.baro_pressure = 251.2231;
-sensor_data.baro_temp = 400.2262;
-uint32_t time = 30515;
-
 char buffer_str[175];
 
-char expected_buffer[] = "time: 30515,accelX: 1,accelY: 2,accelZ:3"
-						",gyroX: 4,gyroY: 5,gyroZ: "
-						"6,magX: 7,magY: 8,magZ: "
-						"9,baro_pres: 251.22,baro_temp: 400.22";
+SENSOR_DATA sensor_data_test[] = 
+{
+#include "inputs/in_buffer.txt"
+};
 
 char *expected_data[] = 
 {
@@ -138,18 +105,14 @@ char *expected_data[] =
 /*------------------------------------------------------------------------------
 Run Tests
 ------------------------------------------------------------------------------*/
-for ( int test_num = 0; test_num < 2; test_num++ )
+for ( int test_num = 0; test_num < 3; test_num++ )
 	{
-    /* Initialize input/output */	
-	dataframe_to_string(&sensor_data, time, &buffer_str[0]);
-	TEST_ASSERT_EQUAL_STRING(expected_data[test_num], buffer_str);
+    /* Initialize input/output */
+	memset(&buffer_str, 0, sizeof(char) * 175); /* Clean buffer string every test */
+	dataframe_to_string( &(sensor_data_test[test_num]), 0, &buffer_str[0]); /* Call a testing function */
+	TEST_ASSERT_EQUAL_STRING(expected_data[test_num], buffer_str); /* Test begins */
     }
-// TEST_ASSERT_EQUAL_STRING(expected_data[0], buffer_str);
 
-// printf(expected_buffer);
-// }
-// // dataframe_to_string(&sensor_data, time, &buffer_str[0]);
-// // TEST_ASSERT_EQUAL_STRING(expected_buffer, buffer_str);
 
 }
 
