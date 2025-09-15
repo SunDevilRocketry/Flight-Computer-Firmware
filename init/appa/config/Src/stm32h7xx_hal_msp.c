447fb706d18f84dac10dd69119582c5360998d56
ETSsound <147210601+ETSsound@users.noreply.github.com>
2025-09-15 15:46:35 -0700

    Fix servo init

--- init/appa/config/Src/stm32h7xx_hal_msp.c
+++ init/appa/config/Src/stm32h7xx_hal_msp.c
@@ -331,7 +331,23 @@ void HAL_TIM_Base_MspInit
 	TIM_HandleTypeDef* htim_base
 	)
 {
-if( htim_base->Instance == TIM4 )
+	if(htim_base->Instance==TIM2)
+	{
+	/* Peripheral clock enable */
+	__HAL_RCC_TIM2_CLK_ENABLE();
+	/* TIM2 interrupt Init */
+	HAL_NVIC_SetPriority(TIM2_IRQn, 0, 0);
+	HAL_NVIC_EnableIRQ(TIM2_IRQn);
+	}
+	else if(htim_base->Instance==TIM3)
+	{
+	/* Peripheral clock enable */
+	__HAL_RCC_TIM3_CLK_ENABLE();
+	/* TIM3 interrupt Init */
+	HAL_NVIC_SetPriority(TIM3_IRQn, 0, 0);
+	HAL_NVIC_EnableIRQ(TIM3_IRQn);
+	}
+	else if(htim_base->Instance==TIM4)
 	{
 	/* Peripheral clock enable */
 	__HAL_RCC_TIM4_CLK_ENABLE();
@@ -422,7 +438,23 @@ void HAL_TIM_Base_MspDeInit
 	TIM_HandleTypeDef* htim_base
 	)
 {
-if( htim_base->Instance == TIM4 )
+if(htim_base->Instance==TIM2)
+	{
+	/* Peripheral clock disable */
+	__HAL_RCC_TIM2_CLK_DISABLE();
+
+	/* TIM2 interrupt DeInit */
+	HAL_NVIC_DisableIRQ(TIM2_IRQn);
+	}
+else if(htim_base->Instance==TIM3)
+	{
+	/* Peripheral clock disable */
+	__HAL_RCC_TIM3_CLK_DISABLE();
+
+	/* TIM3 interrupt DeInit */
+	HAL_NVIC_DisableIRQ(TIM3_IRQn);
+	}
+else if(htim_base->Instance==TIM4)
 	{
 	/* Peripheral clock disable */
 	__HAL_RCC_TIM4_CLK_DISABLE();
