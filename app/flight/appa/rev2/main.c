fb00c88551cdc0c8882c5d73f4385efafd169c2e
ETSsound <147210601+ETSsound@users.noreply.github.com>
2025-12-23 12:28:06 -0700

    Add license terms to APPA

--- app/flight/appa/rev2/main.c
+++ app/flight/appa/rev2/main.c
@@ -10,7 +10,18 @@
 *		one comprehensive & configurable application for the flight computer.
 *
 * CRITICALITY:
-*		NFQ - Non-Flight Qualified	
+*		NFQ - Non-Flight Qualified
+*                                                                             
+* COPYRIGHT:                                                                  
+*       Copyright (c) 2025 Sun Devil Rocketry.                                
+*       All rights reserved.                                                  
+*                                                                             
+*       This software is licensed under terms that can be found in the LICENSE
+*       file in the root directory of this software component.                 
+*       If no LICENSE file comes with this software, it is covered under the   
+*       BSD-3-Clause.                                                          
+*                                                                              
+*       https://opensource.org/license/bsd-3-clause                            
 *
 *******************************************************************************/
 
--- app/flight/appa/rev2/sensor_calibrate.c
+++ app/flight/appa/rev2/sensor_calibrate.c
@@ -9,6 +9,17 @@
 * CRITICALITY:                                                                 *
 *              FQ - Flight Qualified                                           *
 *                                                                              *
+* COPYRIGHT:                                                                   *
+*       Copyright (c) 2025 Sun Devil Rocketry.                                 *
+*       All rights reserved.                                                   *
+*                                                                              *
+*       This software is licensed under terms that can be found in the LICENSE *
+*       file in the root directory of this software component.                 *
+*       If no LICENSE file comes with this software, it is covered under the   *
+*       BSD-3-Clause.                                                          *
+*                                                                              *
+*       https://opensource.org/license/bsd-3-clause                            *
+*                                                                              *
 *******************************************************************************/
 
 /*------------------------------------------------------------------------------
