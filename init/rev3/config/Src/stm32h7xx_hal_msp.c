ae8897a479c39985698bdf242eefa2edf69b5e7e
NArmistead <nicholas.armistead05@gmail.com>
2026-06-19 15:32:22 -0700

    oops, flipped two numbers

--- init/rev3/config/Src/stm32h7xx_hal_msp.c
+++ init/rev3/config/Src/stm32h7xx_hal_msp.c
@@ -958,9 +958,9 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 13749;
+  htim2.Init.Prescaler = 4;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 4;
+  htim2.Init.Period = 13749;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
