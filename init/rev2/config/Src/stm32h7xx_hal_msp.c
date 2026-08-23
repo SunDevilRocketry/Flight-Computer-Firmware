fd512fa1f28f943c5f260f0dafaabd6b7f7962a3
MasterUser43 <epicbinguser43@gmail.com>
2026-08-23 09:38:25 -0700

    hook up LoRa DMA channels and DIO0 IRQ handlers in rev2 config

--- init/rev2/config/Src/stm32h7xx_hal_msp.c
+++ init/rev2/config/Src/stm32h7xx_hal_msp.c
@@ -34,6 +34,12 @@
 #include "main.h"
 #include "error_sdr.h"
 
+/*------------------------------------------------------------------------------
+ External Variables
+------------------------------------------------------------------------------*/
+extern DMA_HandleTypeDef hdma_spi4_rx;
+extern DMA_HandleTypeDef hdma_spi4_tx;
+
 
 /*------------------------------------------------------------------------------
  Function Prototypes 
@@ -345,6 +351,50 @@ else if( hspi->Instance==SPI4 ) /* Flash SPI Initialization */
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF5_SPI4;
     HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+    /* SPI4 DMA Init */
+    __HAL_RCC_DMA1_CLK_ENABLE();
+
+    /* SPI4_RX Init */
+    hdma_spi4_rx.Instance = DMA1_Stream0;
+    hdma_spi4_rx.Init.Request = DMA_REQUEST_SPI4_RX;
+    hdma_spi4_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
+    hdma_spi4_rx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_spi4_rx.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_spi4_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_spi4_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_spi4_rx.Init.Mode = DMA_NORMAL;
+    hdma_spi4_rx.Init.Priority = DMA_PRIORITY_LOW;
+    hdma_spi4_rx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+    if (HAL_DMA_Init(&hdma_spi4_rx) != HAL_OK)
+      {
+      error_fail_fast( ERROR_LORA_SPI_INIT_ERROR );
+      }
+    __HAL_LINKDMA(hspi, hdmarx, hdma_spi4_rx);
+
+    /* SPI4_TX Init */
+    hdma_spi4_tx.Instance = DMA1_Stream1;
+    hdma_spi4_tx.Init.Request = DMA_REQUEST_SPI4_TX;
+    hdma_spi4_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
+    hdma_spi4_tx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_spi4_tx.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_spi4_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_spi4_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_spi4_tx.Init.Mode = DMA_NORMAL;
+    hdma_spi4_tx.Init.Priority = DMA_PRIORITY_LOW;
+    hdma_spi4_tx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+    if (HAL_DMA_Init(&hdma_spi4_tx) != HAL_OK)
+      {
+      error_fail_fast( ERROR_LORA_SPI_INIT_ERROR );
+      }
+    __HAL_LINKDMA(hspi, hdmatx, hdma_spi4_tx);
+
+    /* DMA1_Stream0/1 interrupt Init - priority 3: below SPI4's
+       own IRQ (2) given LoRa is "sub-critical".  */
+    HAL_NVIC_SetPriority(DMA1_Stream0_IRQn, 3, 0);
+    HAL_NVIC_EnableIRQ(DMA1_Stream0_IRQn);
+    HAL_NVIC_SetPriority(DMA1_Stream1_IRQn, 3, 0);
+    HAL_NVIC_EnableIRQ(DMA1_Stream1_IRQn);
 
     /* SPI4 interrupt Init */
     HAL_NVIC_SetPriority(SPI4_IRQn, 2, 0);
